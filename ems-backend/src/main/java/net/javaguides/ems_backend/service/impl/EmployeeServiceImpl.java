139cc5ae68798c0935ef07deaeafc42450501beb
Momcilo-Petkovic <momcilopetkovic27@gmail.com>
2025-02-19 19:18:27 +0100

    Made Get Employee REST API and Not found exception for it

--- ems-backend/src/main/java/net/javaguides/ems_backend/service/impl/EmployeeServiceImpl.java
+++ ems-backend/src/main/java/net/javaguides/ems_backend/service/impl/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package net.javaguides.ems_backend.service.impl;
 import lombok.AllArgsConstructor;
 import net.javaguides.ems_backend.dto.EmployeeDto;
 import net.javaguides.ems_backend.entity.Employee;
+import net.javaguides.ems_backend.exception.ResourceNotFoundException;
 import net.javaguides.ems_backend.mapper.EmployeeMapper;
 import net.javaguides.ems_backend.repository.EmployeeRepository;
 import net.javaguides.ems_backend.service.EmployeeService;
@@ -22,5 +23,12 @@ public class EmployeeServiceImpl implements EmployeeService {
         Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
         Employee savedEmployee = employeeRepository.save(employee);
         return EmployeeMapper.mapToEmployeeDto(savedEmployee);
+    }
+
+    @Override
+    public EmployeeDto getEmployeeById(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee does not exist with given id: " + employeeId));
+        return EmployeeMapper.mapToEmployeeDto(employee);
     }
 }
