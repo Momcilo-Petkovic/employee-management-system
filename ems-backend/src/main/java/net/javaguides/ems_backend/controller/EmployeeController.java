05781d34d1d06bacb308829ae989322bc6256e80
Momcilo <momcilopetkovic27@gmail.com>
2025-02-20 18:50:44 +0100

    Set up Spring backend, starting React

--- ems-backend/src/main/java/net/javaguides/ems_backend/controller/EmployeeController.java
+++ ems-backend/src/main/java/net/javaguides/ems_backend/controller/EmployeeController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/employees")
 
@@ -29,5 +31,26 @@ public class EmployeeController {
     public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable("id") Long employeeId){
         EmployeeDto employeeDto = employeeService.getEmployeeById(employeeId);
         return ResponseEntity.ok(employeeDto);
+    }
+
+    //Build Get Employee Rest API
+    @GetMapping
+    public ResponseEntity<List<EmployeeDto>> getAllEmployees(){
+       List<EmployeeDto> employees = employeeService.getAllEmployees();
+       return ResponseEntity.ok(employees);
+    }
+
+    //Build Get All Employees Rest API
+    @PutMapping("{id}")
+    public ResponseEntity<EmployeeDto> updateEmployee(@PathVariable("id") Long employeeId, @RequestBody EmployeeDto updatedEmployee){
+        EmployeeDto employeeDto = employeeService.updateEmployee(employeeId, updatedEmployee);
+        return ResponseEntity.ok(employeeDto);
+    }
+
+    // Build Delete Employee REST API
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable("id") Long employeeId){
+        employeeService.deleteEmployee(employeeId);
+        return ResponseEntity.ok("Employee deleted successfully!.");
     }
 }
