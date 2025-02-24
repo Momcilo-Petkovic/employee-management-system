ac71a1847cd318547c87c7695dcba9c909b18989
Momcilo <momcilopetkovic27@gmail.com>
2025-02-24 18:11:44 +0100

    Created a react project, made some frontend with Bootstrap, created React ListEmployeeComponent and Displayed Dummy Data and Connected React App with Get All Employees REST API using Axios

---
+++ full-stack/ems/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ListEmployeeComponent from './components/ListEmployeeComponent'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+
+
+  return (
+    <>
+      <ListEmployeeComponent/>
+    </>
+  )
+}
+
+export default App
