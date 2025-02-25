514b6d5d69f592b90efa0e767f47bb26ed3eb369
Momcilo <momcilopetkovic27@gmail.com>
2025-02-25 12:55:40 +0100

    Created header and footer components, Configured Routing in React App, Created EmployeeComponent and Configured route, Add Employee For Handling, connected it to Add Employee REST API, Added Employee Form Validation, Added Update Button Title and Route, Connected React App to Get Employee REST API, Connected React App to update Employee REST API and Implemented Delete Employee Feature in React App

---
+++ full-stack/ems/src/components/HeaderComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HeaderComponent = () => {
+  return (
+    <div>
+        <header>
+            <nav className='navbar navbr-dark bg-dark'>
+                <a className="navbar-brand text-white mx-2" href="https://github.com/Momcilo-Petkovic">Employee Management System</a>
+            </nav>
+        </header> 
+    </div>
+  )
+}
+
+export default HeaderComponent
--- full-stack/ems/src/components/ListEmployeeComponent.jsx
+++ full-stack/ems/src/components/ListEmployeeComponent.jsx
@@ -1,20 +1,45 @@
 import React, {useEffect, useState} from 'react'
-import { listEmployees } from '../services/EmployeeService'
-
+import { deleteEmployee, listEmployees } from '../services/EmployeeService'
+import { useNavigate } from 'react-router-dom'
     
 const ListEmployeeComponent = () => {
         const [employees, setEmployees] = useState([])
 
+        const navigator = useNavigate();
+
         useEffect(() => {
+            getAllEmployees();
+        }, [])
+
+        function getAllEmployees(){
             listEmployees().then((response) => {
                 setEmployees(response.data);
             }).catch(error => {
                 console.error(error);
             })
-        }, [])
+        }
+
+        function addNewEmployee(){
+            navigator('/add-employee')
+        }
+
+        function updateEmployee(id){
+            navigator(`/edit-employee/${id}`)
+        }
+
+        function removeEmployee(id){
+            console.log(id);
+            deleteEmployee(id).then((response) =>{
+                getAllEmployees()
+            }).catch(error => {
+                console.error(error)
+            })
+        }
+
     return (
     <div className='container'>
         <h2 className='text-center'>List of Employees </h2>
+        <button type='button' className='btn btn-primary mb-2' onClick={addNewEmployee}>Add Employee</button>
         <table className='table table-striped table-border'>
             <thead>
                 <tr>
@@ -22,6 +47,7 @@ const ListEmployeeComponent = () => {
                     <th>Employee First Name</th>
                     <th>Employee Last Name</th>
                     <th>Employee Email  Id </th>
+                    <th>Actions</th>
                 </tr>
             </thead>
             <tbody>
@@ -31,6 +57,13 @@ const ListEmployeeComponent = () => {
                             <td>{employee.firstName}</td>
                             <td>{employee.lastName}</td>
                             <td>{employee.email}</td>
+                            <td>
+                                <button className='btn btn-info'onClick={() =>  updateEmployee(employee.id)}>Update</button>
+                            </td>
+                            <td>
+                                <button className='btn btn-danger'onClick={() =>  removeEmployee(employee.id)}
+                                    style={{marginLeft: '5px'}}>Delete</button>
+                            </td>
                     </tr>)                               
                    } 
             </tbody>
