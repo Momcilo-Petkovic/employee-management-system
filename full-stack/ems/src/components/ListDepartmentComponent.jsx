bed28e7298d8ae5cf883d73b34cec8c0885254fe
Momcilo <momcilopetkovic27@gmail.com>
2025-03-03 21:02:01 +0100

    Fixed some stuff

--- full-stack/ems/src/components/ListDepartmentComponent.jsx
+++ full-stack/ems/src/components/ListDepartmentComponent.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getAllDepartments } from '../services/DepartmentService';
 
 const ListDepartmentComponent = () => {
+
+  const [departments, setDepartments] = useState([]);
+
+  useEffect(() => {
+    getAllDepartments().then((response)=>{
+      console.log(response.data);
+      setDepartments(response.data);
+    }).catch(error => {
+      console.error(error);
+    })
+  }, [] )
   return (
-    <div>ListDepartmentComponent</div>
+    <div className='container'>
+        <h2 className='text-center'>List of Departments </h2>
+            <table className='table table-striped table-border'>
+            <thead>
+                <tr>
+                    <th>Department Id</th>
+                    <th>Department Name</th>
+                    <th>Department Description</th>
+
+                </tr>
+            </thead>
+            <tbody>
+                   {
+                    departments.map(department =>  <tr key={department.id}>
+                            <td>{department.id}</td>
+                            <td>{department.departmentName}</td>
+                            <td>{department.departmentDescription}</td>
+                    </tr>)                               
+                   } 
+            </tbody>
+        </table>
+    </div>
   )
 }
 
